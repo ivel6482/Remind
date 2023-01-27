fe0d6c366637570d3c2cf7dbe5aea23170f3c806
Ilder Vélez <38200521+ivel6482@users.noreply.github.com>
2023-01-26 22:23:12 -0400

    Add hero component

--- client/src/components/UI/Hero.tsx
+++ client/src/components/UI/Hero.tsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom"
+
 export function Hero({ exercise }: {
   exercise: any
 }) {
 
   return (
-    <section>
+    <section className="grid max-w-5xl grid-cols-1 mx-auto md:grid-cols-2 md:mt-10">
       <img src={exercise.thumbnail} alt={exercise.title} />
-      <div className="p-5 space-y-4">
+      <div className="p-5 space-y-4 md:pt-0">
         <h3 className="text-2xl font-semibold">{exercise.title}</h3>
         <p className="text-lg leading-8 text-gray-700">{exercise.description}</p>
-        <div></div>
+        <div><Link to={`/exercises/${exercise._id}`}>See exercise</Link></div>
       </div>
     </section>
   )
